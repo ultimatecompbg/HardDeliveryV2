fc81ff0d5593b1b1f21e74df130048a2fdd99c59
ultimatecompbg <baystancho0@gmail.com>
2024-06-18 05:17:04 +0300

    Admin controller

---
+++ HardDeliveryNew/Services/AdminService.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using HardDelivery.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace HardDelivery.Services
+{
+    public interface IAdminService
+    {
+        Task<bool> ChangeUserRoleAsync(string userId, string newRole);
+    }
+
+    public class AdminService : IAdminService
+    {
+        private readonly UserManager<User> _userManager;
+
+        public AdminService(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<bool> ChangeUserRoleAsync(string userId, string newRole)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+                return false;
+
+            var currentRoles = await _userManager.GetRolesAsync(user);
+            await _userManager.RemoveFromRolesAsync(user, currentRoles.ToArray());
+            await _userManager.AddToRoleAsync(user, newRole);
+
+            return true;
+        }
+    }
+}
